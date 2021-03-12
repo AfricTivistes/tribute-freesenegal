b4a7c59d143b4e2b56fb05e4d0fa5caecdc9a1c3
Mamadou Diagne <genova@dofbi.com>
2021-03-12 11:52:24 +0000

    Feat: form

--- functions/github.js
+++ functions/github.js
@@ -4,22 +4,24 @@ const moment = require('moment');
 
 const { GITHUB_TOKEN, GITHUB_USER, GITHUB_REPO } = process.env
 
-const gitpush = (data) => {
+const gitpush = async(data) => {
 
-    const publisher = new GitHubPublisher(GITHUB_TOKEN, GITHUB_USER, GITHUB_REPO);
-    const path = "content/tribute/"
-    
-    const { tribute, author, twitter, facebook } = data.data
-    const { created_at: date } = data
-    let filename = `${path}tribute-${moment(date).format("YYYY-MM-DD-HH-mm")}.md`
-    let filecontent = `---\ntitle: \ndate: ${date}\nauthor: ${author}\ntwitter: ${twitter}\nfacebook: ${facebook}\n---\n\n${tribute}`
+    return new Promise((resolve) => {
 
-    publisher.publish(filename, filecontent).then(function (result) {
-        // If "result" is truthy then the post was successfully published
-        console.log('ok')
-    });
+        const publisher = new GitHubPublisher(GITHUB_TOKEN, GITHUB_USER, GITHUB_REPO);
+        const path = "content/tribute/"
+        
+        const { tribute, author, twitter, facebook } = data.data
+        const { created_at: date } = data
+        let filename = `${path}tribute-${moment(date).format("YYYY-MM-DD-HH-mm")}.md`
+        let filecontent = `---\ntitle: \ndate: ${date}\nauthor: ${author}\ntwitter: ${twitter}\nfacebook: ${facebook}\n---\n\n${tribute}`
 
-    
+        publisher.publish(filename, filecontent).then(function (result) {
+            // If "result" is truthy then the post was successfully published
+            resolve("Enregister")
+        });
+
+    })
 }
 
 module.exports = gitpush
--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,12 @@
 const gitpush = require('./github')
 
-exports.handler = async event => {
+exports.handler = async (event, context, callback) => {
 
     const data = JSON.parse(event.body).payload
-    gitpush(data)
+    gitpush(data).then(
+        result => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({message: result})
+    })})
 }
