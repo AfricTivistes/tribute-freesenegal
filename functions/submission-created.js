1455c4845683dc83ef5cc9a48f2a999729f7f433
Mamadou Diagne <genova@dofbi.com>
2021-03-12 13:11:57 +0000

    Feat: fix form

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,7 +3,7 @@ const gitpush = require('./github')
 exports.handler = async (event, context, callback) => {
 
     const data = JSON.parse(event.body).payload
-    gitpush(data).then(
+    await gitpush(data).then(
         result => {
             callback(null, {
                 statusCode: 200,
